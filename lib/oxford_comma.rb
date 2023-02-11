9a5d65999f4e0bf0834062ddc516036d8655287f
Skanner33 <mathewmageto@gmail.com>
2023-02-11 19:02:30 +0300

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  long = array.size
+  if long == 1
+    return array.join
+  elsif long == 2
+    lastelem = array.pop
+    array.push(" and ")
+    array.push (lastelem)
+    return array.join
+  elsif long >= 3
+    lastelem = array.pop
+    array.push("and ")
+    cad = array.join(", ")
+    cad = cad + lastelem
+    return cad
+  end
 end
